8fb8382c0d27f96c6fd0ad203291163b9912871e
hassanoussidi <assan1@outlook.es>
2018-02-26 13:57:56 +0100

    modificacar- comentarios

--- Estructura_for_1/Estructura_for_1.ino
+++ Estructura_for_1/Estructura_for_1.ino
@@ -1,20 +1,22 @@
 /*******************************************************************************    
-                                             
+        muestra  en la pantalla      
+        ( Ahora contare de 0 a 11 >>> 0-1-2-3-4-5-6-7-8-9-10-11-)                          
 *******************************************************************************/
  
 int contar = 11;
-void setup ()                // run once, when the sketch starts
-{Serial.begin
-  (9600);       // set up Serial library at 9600 bps
-  Serial.print ( "Ahora contaré de 0 a");
+void setup ()         // se ejecuta una vez
+{
+  Serial.begin(9600); //activa el puerto serie y fija la velocidad en 9600 bps
+  Serial.print ( "Ahora contare de 0 a "); 
   Serial.println (contar);
-  for (int i = 0; i <= contar; i ++)
-  {Serial.print
-    (i);
-    Serial.print ( "-");
-   
-}}
-Void loop ()     // we need this to be here even though its empty
+  
+  for (int i = 0; i <= contar; i ++){
+    Serial.print (i);
+    Serial.print ( "-"); } 
+     
+}
 
-{}
+void loop() {} // se ejecuta cíclicamente
+
+
 
